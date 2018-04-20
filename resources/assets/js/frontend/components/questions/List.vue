c0ab1c92e1a08a49208dd9c9c37b944af2ca9d7e
lupacescueduard <lupacescueduard@yahoo.com>
2018-04-21 01:21:19 +0300

    List

--- resources/assets/js/frontend/components/questions/List.vue
+++ resources/assets/js/frontend/components/questions/List.vue
@@ -1,6 +1,20 @@
 <template>
     <div class="col-sm-12 col-xl-12">
         <div class="card">
+            <div class="switch-container col-md-12">
+                <button type="button" class="col-md-4 float-left btn btn-outline-secondary btn-md btn-">Recente</button>
+                <button type="button" class="col-md-4 float-left btn btn-outline-primary btn-md btn-">Dupa interese</button>
+                <div class="btn-group show">
+                    <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" @click="showInstitutions = !showInstitutions">
+                        Selecteaza institutia
+                    </button>
+                    <div class="dropdown-menu" v-bind:class="{show: showInstitutions}" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 35px, 0px); top: 0px; left: 0px; will-change: transform;">
+                        <a class="dropdown-item" href="#" v-for="inst in institutions" :key="institution.id">
+                            {{ institution.name }}
+                        </a>
+                    </div>
+                </div>
+            </div>
             <div class="card-header">
                 <i class="fa fa-align-justify"></i> Intrebari
                 <small>cele mai recente</small>
@@ -25,7 +39,9 @@
         },
         data() {
             return {
-                list: []
+                list: [],
+                currentView: 1,
+                showInstitutions: false
             }
         },
         async created() {
@@ -36,3 +52,8 @@
 
     }
 </script>
+<style lang="scss">
+    .switch-container {
+        margin: 10px;
+    }
+</style>
